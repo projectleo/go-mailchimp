9ff116b2ce22781c245f2f04aaf317c440718f32
Anastasios Daris <t.daris@7linternational.com>
2017-11-16 17:25:01 +0200

    Add a remove subscription function

---
+++ remove_subscription.go
@@ -0,0 +1,54 @@
+package mailchimp
+
+import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/RichardKnop/go-mailchimp/status"
+)
+
+// UpdateSubscription ...
+func (c *Client) RemoveSubscription(listID string, email string, mergeFields map[string]interface{}) (*MemberResponse, error) {
+	// Hash email
+	emailMD5 := fmt.Sprintf("%x", md5.Sum([]byte(email)))
+	// Make request
+	params := map[string]interface{}{
+		"email_address": email,
+		"status":        status.Subscribed,
+		"merge_fields":  mergeFields,
+	}
+	resp, err := c.do(
+		"DELETE",
+		fmt.Sprintf("/lists/%s/members/%s", listID, emailMD5),
+		&params,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Allow any success status (2xx)
+	if resp.StatusCode/100 == 2 {
+		// Unmarshal response into MemberResponse struct
+		memberResponse := new(MemberResponse)
+		if err := json.Unmarshal(data, memberResponse); err != nil {
+			return nil, err
+		}
+		return memberResponse, nil
+	}
+
+	// Request failed
+	errorResponse, err := extractError(data)
+	if err != nil {
+		return nil, err
+	}
+	return nil, errorResponse
+}
