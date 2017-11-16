e69b2a84a99853b5816069bd24fe1368a498fe8b
Anastasios Daris <t.daris@7linternational.com>
2017-11-16 17:34:52 +0200

    Forgot to update the client interface

--- client_interface.go
+++ client_interface.go
@@ -10,6 +10,7 @@ type ClientInterface interface {
 	CheckSubscription(listID string, email string) (*MemberResponse, error)
 	Subscribe(listID string, email string, mergeFields map[string]interface{}) (*MemberResponse, error)
 	UpdateSubscription(listID string, email string, mergeFields map[string]interface{}) (*MemberResponse, error)
+	RemoveSubscription(listID string, email string, mergeFields map[string]interface{}) (*MemberResponse, error)
 	SetBaseURL(baseURL *url.URL)
 	GetBaseURL() *url.URL
 }
